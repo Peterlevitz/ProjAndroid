5c6d465cf63c16f6df704da08872cd5312275631
Leonardo Peterlevitz <leocavichia@gmail.com>
2020-10-31 22:02:04 -0300

    criados forms de cadastro de user e serie

--- lib/cad_serie.dart
+++ lib/cad_serie.dart
@@ -7,63 +7,144 @@ class CadSerie extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Text('Menu Principal'),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('Menu Principal'),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
               ),
-              ListTile(
-                title: Text('Lista de séries'),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) => new TelaPrincipal()));
+            ),
+            ListTile(
+              title: Text('Lista de séries'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new TelaPrincipal()));
+              },
+            ),
+            ListTile(
+              title: Text('Cadastro de séries'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Ranking de séries'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new RankSerie()));
+              },
+            ),
+            ListTile(
+              title: Text('Sair'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) => new MyApp()));
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Cadastro de séries"),
+      ),
+      body: FormCadSerie(),
+    );
+  }
+}
+
+class FormCadSerie extends StatefulWidget {
+  @override
+  FormCadSerieState createState() {
+    return FormCadSerieState();
+  }
+}
+
+class FormCadSerieState extends State<FormCadSerie> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Text('Título da série:'),
+              TextFormField(validator: (value) {
+                if (value.isEmpty) {
+                  return 'Campo de preenchimento obrigatório';
+                }
+                return null;
+              }),
+              Text('Sinopse:'),
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Campo de preenchimento obrigatório';
+                  }
+                  return null;
                 },
+                maxLines: 5,
               ),
-              ListTile(
-                title: Text('Cadastro de séries'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Ranking de séries'),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) => new RankSerie()));
-                },
-              ),
-              ListTile(
-                title: Text('Sair'),
-                onTap: () {
-                  Navigator.pop(context);
+              Text('Duração dos episódios'),
+              TextFormField(validator: (value) {
+                if (value.isEmpty) {
+                  return 'Campo de preenchimento obrigatório';
+                }
+                return null;
+              }),
+              Text('Número de episódios:'),
+              TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Campo de preenchimento obrigatório';
+                    }
+                    return null;
+                  }),
+              Text('Temporadas:'),
+              TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Campo de preenchimento obrigatório';
+                    }
+                    return null;
+                  }),
+              Text('Faixa etária:'),
+              TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Campo de preenchimento obrigatório';
+                    }
+                    return null;
+                  }),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Processando...')));
+                    }
+                  },
+                  child: Text('Cadastrar')),
+              ElevatedButton(
+                onPressed: () {
                   Navigator.push(context,
                       new MaterialPageRoute(builder: (context) => new MyApp()));
                 },
+                child: Text('Voltar'),
               ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: Text("Cadastro de séries"),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (context) => new MyApp()));
-            },
-            child: Text('Voltar'),
-          ),
-        ));
+            ])));
   }
 }
--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -5,17 +5,86 @@ class TelaCadastro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Tela de cadastro"),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (context) => new MyApp()));
-            },
-            child: Text('Voltar'),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text("Tela de cadastro"),
+      ),
+      body: FormCadUser(),
+    );
+  }
+}
+
+class FormCadUser extends StatefulWidget {
+  @override
+  FormCadUserState createState() {
+    return FormCadUserState();
+  }
+}
+
+class FormCadUserState extends State<FormCadUser> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Text('Nome completo:'),
+              TextFormField(validator: (value) {
+                if (value.isEmpty) {
+                  return 'Campo de preenchimento obrigatório';
+                }
+                return null;
+              }),
+              Text('Email:'),
+              TextFormField(validator: (value) {
+                if (value.isEmpty) {
+                  return 'Campo de preenchimento obrigatório';
+                }
+                return null;
+              }),
+              Text('Nome de usuário:'),
+              TextFormField(validator: (value) {
+                if (value.isEmpty) {
+                  return 'Campo de preenchimento obrigatório';
+                }
+                return null;
+              }),
+              Text('Senha:'),
+              TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Campo de preenchimento obrigatório';
+                    }
+                    return null;
+                  }),
+              Text('Confirmar Senha:'),
+              TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Campo de preenchimento obrigatório';
+                    }
+                    return null;
+                  }),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Processando...')));
+                    }
+                  },
+                  child: Text('Cadastrar')),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => new MyApp()));
+                },
+                child: Text('Voltar'),
+              ),
+            ])));
   }
 }
